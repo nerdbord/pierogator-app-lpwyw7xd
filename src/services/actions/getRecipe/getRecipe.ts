c1d139f11c5dd926b620ea5360ddbde6dc661eb0
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-12-10 00:36:05 +0100

    chore: create getRecipe by ID

---
+++ src/services/actions/getRecipe/getRecipe.ts
@@ -0,0 +1,27 @@
+﻿'use server'
+
+export async function getRecipe(id: string) {
+  try {
+    const response = await fetch(
+      `https://training.nerdbord.io/api/v1/pierogator/dumpling-recipes/${id}`,
+      {
+        method: 'GET',
+        headers: {
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          Pragma: 'no-cache',
+          Expires: '0',
+        },
+      },
+    )
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const res = await response.json()
+    return res
+  } catch (error) {
+    console.error('Error fetching my dumplings:', error)
+    throw new Error(`HTTP error! status: ${error}`)
+  }
+}
