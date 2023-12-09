033f1c4e3997443e4a395d0b51db26217d3051a7
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-12-09 01:20:52 +0100

    feat: added ingredients managing view

--- src/app/(dumpligator)/page.tsx
+++ src/app/(dumpligator)/page.tsx
@@ -1,54 +1,88 @@
 ﻿'use client'
 import React from 'react'
 import TextField from '@/components/TextField/TextField'
-import { Unlock } from '../../assets/icons/Unlock/Unlock'
-import { Lock } from '../../assets/icons/Lock/Lock'
 import { Button } from '@/components/Button/Button'
 import Loader from '@/components/Loader/Loader'
 import { SectionHeader } from '@/components/SectionHeader/SectionHeader'
 import useDumplingStore from '@/store/useDumplingStore'
 import { Accordion } from '@/components/Accordion/Accordion'
-
+import styles from './page.module.scss'
 const sections = [
   {
-    title: "Ciasto",
+    title: 'Ciasto',
     items: [
-      "2 szklanki mąki pszennej",
-      "1/2 szklanki ciepłej wody",
-      "1 łyżka oleju",
-      "Szczypta soli",
+      '2 szklanki mąki pszennej',
+      '1/2 szklanki ciepłej wody',
+      '1 łyżka oleju',
+      'Szczypta soli',
     ],
   },
   {
-    title: "Farsz",
+    title: 'Farsz',
     items: [
-      "2 szklanki kiszonej kapusty",
-      "1 szklanka suszonych grzybów (np. borowiki, podgrzybki)",
-      "1 cebula",
-      "2 łyżki oleju",
-      "Sól, pieprz do smaku",
+      '2 szklanki kiszonej kapusty',
+      '1 szklanka suszonych grzybów (np. borowiki, podgrzybki)',
+      '1 cebula',
+      '2 łyżki oleju',
+      'Sól, pieprz do smaku',
     ],
   },
-];
+]
 import { Card } from '@/components/Card/Card'
+import TextFieldSingle from '@/components/TextFieldSingle/TextFieldSingle'
 
 const Dumpligator = () => {
   const { tmp, setTmp } = useDumplingStore()
   return (
-    <div>
-      <SectionHeader>PIEROGATOR MORDO</SectionHeader>
+    <div className={styles.container}>
+      <div className={styles.headerWrapper}>
+        <SectionHeader>Składniki</SectionHeader>
+        <div className={styles.buttonWrapper}>
+          {tmp && <Loader />}
+          <Button onClick={() => setTmp(!tmp)}>Generuj</Button>
+        </div>
+      </div>
       <TextField
-        onChange={() => console.log('huj')}
-        // iconState="none"    <-------
-        placeholder="wegańskie ciasto na pszennej mące uniwersalnej"
+        placeholder="wpisz, wygeneruj lub zostaw puste"
         label="Ciasto"
       />
-
-      {tmp && <Loader />}
-      <Button onClick={() => setTmp(!tmp)}>Generuj</Button>
+      <TextField
+        placeholder="wpisz, wygeneruj lub zostaw puste"
+        label="Nadzienie"
+      />
+      <TextField
+        placeholder="wpisz, wygeneruj lub zostaw puste"
+        label="Składniki"
+      />
+      <div className={styles.headerWrapper}>
+        <SectionHeader>Pieróg</SectionHeader>
+        <div className={styles.buttonWrapper}>
+          {tmp && <Loader />}
+          <Button onClick={() => setTmp(!tmp)}>Generuj</Button>
+        </div>
+      </div>
+      {tmp && (
+        <>
+          <Card
+            item={{
+              name: '',
+              img: 'https://i.imgur.com/Odcmv1g.png',
+              url: 'www.url-do-pieroga.ru',
+            }}
+          />
+          <TextFieldSingle
+            label="Nazwa"
+            iconState="none"
+            value="Piróg Piroga"
+          />
+          <Button variant="action">
+            Zapisz i przejdź do tworzenia przepisu
+          </Button>
+        </>
+      )}
+      {/* <Button onClick={() => setTmp(!tmp)}>Generuj</Button>)}
       <Button variant="action">Zapisz i przejdź do tworzenia przepisu</Button>
       <Accordion header={'Składniki'} sections={sections} />
-
       <Card
         item={{
           name: 'Piróg',
@@ -56,7 +90,7 @@ const Dumpligator = () => {
           url: 'www.url-do-pieroga.ru',
         }}
         withActions
-      />
+      /> */}
     </div>
   )
 }
