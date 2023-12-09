84b0efe992e402be400b2f7da726be9bf1a428ca
JKapturkiewicz <kuba.kapek@gmail.com>
2023-12-09 11:37:54 +0100

    wip: update textFieldProps

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -10,7 +10,7 @@ interface TextFieldProps {
   name?: string
   placeholder?: string
   iconState?: 'none' | 'lock' | 'unlock'
-  onChange: ChangeEventHandler<HTMLTextAreaElement>;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
   label?: string
   value?: string
 }
