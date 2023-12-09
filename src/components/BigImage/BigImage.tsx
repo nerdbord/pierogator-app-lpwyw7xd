19d950d316fc5e4d70ce3f13973453858a366028
JKapturkiewicz <kuba.kapek@gmail.com>
2023-12-09 18:30:01 +0100

    wip: create BigImage component

---
+++ src/components/BigImage/BigImage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './BigImage.module.scss'
+
+function BigImage({ src }: { src: string }) {
+  return (
+    <div className={styles.image}>
+      <Image
+        src={src}
+        alt="Dumpling image"
+        fill
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  )
+}
+
+export default BigImage
