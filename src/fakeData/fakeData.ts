1696c5439a0f1dc00386bf081cc654462e24bd41
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-12-10 17:09:43 +0100

    feat: added recipes generator and presist zustand

--- src/fakeData/fakeData.ts
+++ src/fakeData/fakeData.ts
@@ -1,68 +1,196 @@
-export const skladniki = [
-    {
-      title: 'Ciasto',
-      items: [
-        '2 szklanki mąki pszennej',
-        '1/2 szklanki ciepłej wody',
-        '1 łyżka oleju',
-        'Szczypta soli',
-      ],
-    },
-    {
-      title: 'Farsz',
-      items: [
-        '2 szklanki kiszonej kapusty',
-        '1 szklanka suszonych grzybów (np. borowiki, podgrzybki)',
-        '1 cebula',
-        '2 łyżki oleju',
-        'Sól, pieprz do smaku',
-      ],
-    },
-  ]
-  
-  export const przygotowanie = [
-    {
-      title: 'Ciasto',
-      items: [
-        '1. W misce wymieszaj mąkę z solą. Dodaj olej i stopniowo wlewaj ciepłą wodę, mieszając do uzyskania jednolitej konsystencji.',
-        '2. Wyłóż ciasto na stolnicę i wyrób dokładnie około 5 minut, aż będzie gładkie i elastyczne.',
-        '3. Odkryj ciasto ściereczką i pozostaw na około 30 minut.',
-      ],
-    },
-    {
-      title: 'Farsz',
-      items: [
-        '1. Przygotowanie grzybów: Grzyby namocz w ciepłej wodzie na około 20 minut, następnie ocedź i drobno posiekaj.',
-        '2. Pokrój cebulę: Na patelni rozgrzej olej, dodaj drobno posiekaną cebulę i smaż do zeszklenia.',
-        '3. Dokładnie grzyby: Do cebuli dodaj grzyby i smaż wszystko razem przez kilka minut.',
-        '4. Dodaj kapustę: Do patelni z cebulą, przypraw solą i pieprzem, a następnie dusz wszystko razem przez około 20 minut. Ostudź do ostygnięcia.',
-      ],
-    },
-    {
-      title: 'Formowanie i gotowanie pierogów',
-      items: [
-        '1. Rozwałkowanie ciasta: Rozwałkuj ciasto na cienki placek, a następnie wykrawaj kręgi przy pomocy szklanki.',
-        '2. Nadziewanie pierogów: Na każdy krąg nakładaj łyżeczkę farszu, zlepij brzegi formując pierogi.',
-        '3. Gotowanie pierogów: W dużym garnku zagotuj wodę z solą. Wrzuć partiami pierogi i gotuj do ich wypłynięcia, po czym pozostaw na powierzchni wody, co oznaczać zwykle około 3-5 minut.',
-        '4. Wyjmowanie pierogów: Wyjmij pierogi łyżką cedzakową i odsącz z wody.',
-      ],
-    },
-  ]
-  
-  export const podawanie = [
-    {
-      title: '',
-      items: [
-        'Podawaj pierogi na ciepło, opcjonalnie możesz je polać roztopionym masłem lub zrumienić na patelni dla chrupiącej skórki.',
-      ],
-    },
-  ]
+type FakeDatabaseType = {
+  [key: string]: {
+    name: string;
+    quantity: string;
+  }[][] | string[][];
+};
 
-  export const defaultValues = {
-    "ingredientsDough": "|NEWLINE|1. mąka pszenna 500g\n |NEWLINE|2. woda 200ml\n |NEWLINE|3. sól 1 łyżeczka\n |NEWLINE|4. olej 2 łyżki",
-    "preparationDough": "|NEWLINE|1. W misce wymieszaj mąkę pszenną, sól i olej.\n|NEWLINE|2. Stopniowo dodawaj wodę, ciągle mieszając, aż powstanie jednolite ciasto.\n|NEWLINE|3. Gdy składniki dobrze się połączą, wyłóż ciasto na stolnicę i zagnieć je przez kilka minut.\n|NEWLINE|4. Gotowe ciasto podziel na mniejsze kawałki i rozwałkuj każdy z nich na cienki placek.\n|NEWLINE|5. Na tak przygotowane placek nałóż farsz i zlep brzegi, tworząc pierogi.\n|NEWLINE|6. Gotowe pierogi gotuj w osolonym wrzątku przez około 5-7 minut, aż będą miękkie i dobrze ugotowane.",
-    "ingredientsFilling": "|NEWLINE|1. Ser, 500g\n |NEWLINE|2. Cebula, 2 sztuki\n |NEWLINE|3. Mięso mielone, 500g\n |NEWLINE|4. Pieprz, do smaku",
-    "preparationFilling": "|NEWLINE|1. Na patelni podsmaż cebulę, aż się zeszkli.\n|NEWLINE|2. Dodaj mięso mielone i smaż, aż będzie dobrze przyrumienione.\n|NEWLINE|3. Dopraw farsz solą i pieprzem.\n|NEWLINE|4. Ostudź farsz i wymieszaj z serem.",
-    "cookingMethod": "|NEWLINE|1. Wytnij okręgi z ciasta pierogowego.\n|NEWLINE|2. Nałóż na każdy okrągłej kawałek farszu.\n|NEWLINE|3. Zlep brzegi ciasta, tworząc pierogi.\n|NEWLINE|4. Gotuj pierogi w osolonej wodzie przez około 5-7 minut.\n|NEWLINE|5. Odcedź pierogi i podawaj na talerzach.",
-    "servingMethod": "|NEWLINE|1. Podawaj gorące pierogi na talerzach.\n|NEWLINE|2. Możesz podać je z sosem pomidorowym lub śmietaną.\n|NEWLINE|3. Posyp pierogi świeżo posiekaną natką pietruszki."
+export const fakeDatabase: FakeDatabaseType = {
+  dough: [
+    [
+      { name: 'Mąka pszenna', quantity: '2 szklanki' },
+      { name: 'Woda ciepła', quantity: '1 szklanka' },
+      { name: 'Drożdże świeże', quantity: '25g' },
+      { name: 'Oliwa z oliwek', quantity: '2 łyżki' },
+      { name: 'Szczypta soli', quantity: 'wg gustu' },
+    ],
+    [
+      { name: 'Mąka żytnia', quantity: '3 szklanki' },
+      { name: 'Mleko', quantity: '1/2 szklanki' },
+      { name: 'Masło', quantity: '50g' },
+      { name: 'Cukier', quantity: '1 łyżka' },
+      { name: 'Sól', quantity: '1 łyżeczka' },
+    ],
+    [
+      { name: 'Mąka orkiszowa', quantity: '2.5 szklanki' },
+      { name: 'Woda mineralna', quantity: '1.5 szklanki' },
+      { name: 'Olej roślinny', quantity: '3 łyżki' },
+      { name: 'Drożdże instant', quantity: '30g' },
+      { name: 'Cukier waniliowy', quantity: '1 łyżeczka' },
+    ],
+    [
+      { name: 'Mąka kukurydziana', quantity: '2 szklanki' },
+      { name: 'Jogurt naturalny', quantity: '1 szklanka' },
+      { name: 'Jajko', quantity: '1 sztuka' },
+      { name: 'Proszek do pieczenia', quantity: '1 łyżeczka' },
+      { name: 'Miód', quantity: '2 łyżki' },
+    ],
+    [
+      { name: 'Mąka graham', quantity: '3 szklanki' },
+      { name: 'Kefir', quantity: '1 szklanka' },
+      { name: 'Drożdże piekarnicze', quantity: '15g' },
+      { name: 'Oliwa z oliwek extra virgin', quantity: '4 łyżki' },
+      { name: 'Sól morska', quantity: '1 łyżka' },
+    ],
+  ],
+  filling: [
+    [
+      { name: 'Ser biały', quantity: '500g' },
+      { name: 'Ziemniaki ugotowane', quantity: '1kg' },
+      { name: 'Cebula drobno posiekana', quantity: '2 sztuki' },
+      { name: 'Pieprz czarny mielony', quantity: 'do smaku' },
+      { name: 'Sól', quantity: 'do smaku' },
+    ],
+    [
+      { name: 'Mięso mielone', quantity: '500g' },
+      { name: 'Pieczarki', quantity: '250g' },
+      { name: 'Cebula', quantity: '1 sztuka' },
+      { name: 'Czosnek', quantity: '3 ząbki' },
+    ],
+    [
+      { name: 'Kapusta kiszona', quantity: '600g' },
+      { name: 'Grzyby suszone', quantity: '100g' },
+      { name: 'Cebula', quantity: '1 sztuka' },
+      { name: 'Marchew', quantity: '1 sztuka' },
+      { name: 'Olej', quantity: '2 łyżki' },
+    ],
+    [
+      { name: 'Ricotta', quantity: '400g' },
+      { name: 'Szpinak', quantity: '200g' },
+      { name: 'Orzechy włoskie', quantity: '50g' },
+      { name: 'Parmezan', quantity: '50g' },
+    ],
+    [
+      { name: 'Jabłko', quantity: '3 sztuki' },
+      { name: 'Cukier', quantity: '100g' },
+      { name: 'Cynamon', quantity: '1 łyżeczka' },
+      { name: 'Rodzynki', quantity: '50g' },
+    ]
+  ],
+  dough_preparation: [
+    [
+      'Wymieszaj mąkę z drożdżami i solą.',
+      'Dodaj ciepłą wodę i oliwę, wyrabiaj ciasto do uzyskania gładkości.',
+      'Odstaw ciasto w ciepłe miejsce do wyrośnięcia na około 30 minut.'
+    ],
+    [
+      'Połącz mąkę, sól i cukier w dużej misce.',
+      'Dodaj stopione masło i mieszaj, aż powstanie jednolita masa.',
+      'Stopniowo dodawaj wodę, wyrabiając ciasto, aż będzie elastyczne.'
+    ],
+    [
+      'W małej misce wymieszaj drożdże z letnią wodą, aż się rozpuszczą.',
+      'W dużej misce wymieszaj mąkę, sól, dodaj rozmieszczone drożdże.',
+      'Wyrabiaj ciasto, aż będzie gładkie, następnie przykryj i odstaw do wyrośnięcia.'
+    ],
+    [
+      'Wymieszaj mąkę orkiszową i sól w misce.',
+      'Dodaj ciepłe mleko i rozpuszczone drożdże, mieszaj do połączenia składników.',
+      'Odstaw ciasto do wyrośnięcia w ciepłym miejscu na 45 minut.'
+    ],
+    [
+      'W dużej misce wymieszaj mąkę, sól i cukier.',
+      'Dodaj zimną wodę i szybko wyrabiaj ciasto, aż zacznie odchodzić od rąk.',
+      'Uformuj kulkę z ciasta, owiń folią i odstaw do lodówki na co najmniej 1 godzinę.'
+    ]
+  ],
+  filling_preparation: [
+    [
+      'Ziemniaki ugotuj, a następnie ugnieć na puree.',
+      'Cebulę podsmaż na złoty kolor i dodaj do serka.',
+      'Do masy serowo-ziemniaczanej dodaj sól i pieprz do smaku, wymieszaj.'
+    ],
+    [
+      'Mięso mielone smaż na patelni do zrumienienia.',
+      'Dodaj posiekaną cebulę i czosnek, smaż do miękkości.',
+      'Przypraw mięso solą, pieprzem i ziołami, odstaw do ostygnięcia.'
+    ],
+    [
+      'Kapustę drobno poszatkuj i zalej wrzątkiem.',
+      'Dodaj starte jabłko i cebulę, smaż na patelni.',
+      'Dopraw kapustę solą, pieprzem i odrobiną cukru, mieszaj do połączenia składników.'
+    ],
+    [
+      'Grzyby namocz w ciepłej wodzie, a następnie drobno posiekaj.',
+      'Cebulę zeszklij na maśle, dodaj grzyby, smaż do odparowania wody.',
+      'Dodaj natkę pietruszki i przyprawy, wymieszaj wszystko razem.'
+    ],
+    [
+      'Ser biały wymieszaj z drobno posiekanym koperkiem.',
+      'Dodaj posiekaną wędzoną rybę i wymieszaj do uzyskania jednolitej masy.',
+      'Dopraw nadzienie solą, pieprzem i sokiem z cytryny.'
+    ]
+  ],
+  cooking: [
+    [
+      'Na stolnicy posypanej mąką rozwałkuj ciasto na cienkie placki.',
+      'Na każdy placek nałóż porcję farszu.',
+      'Zlep brzegi ciasta, formując pierogi.',
+      'Gotuj pierogi w osolonej wodzie do wypłynięcia na powierzchnię.'
+    ],
+    [
+      'Rozwałkuj ciasto na grubość około 3 mm.',
+      'Wytnij kółka przy pomocy szklanki lub foremki.',
+      'Na środek każdego kółka nałóż łyżeczkę farszu.',
+      'Zlep brzegi, tworząc półksiężyce.',
+      'Gotuj pierogi w dużym garnku z osoloną wodą przez około 5 minut.'
+    ],
+    [
+      'Rozgrzej piekarnik do 180 stopni Celsjusza.',
+      'Uformuj pierogi i ułóż je na blaszce wyłożonej papierem do pieczenia.',
+      'Piecz pierogi przez 20-25 minut, aż będą złociste.'
+    ],
+    [
+      'Ciasto rozwałkuj na grubość 2 mm.',
+      'Wykrawaj kwadraty, na środek każdego nałóż farsz.',
+      'Formuj pierogi, składając kwadraty na pół, by utworzyć trójkąty.',
+      'Gotuj pierogi w bulionie warzywnym przez 6-7 minut.'
+    ],
+    [
+      'Rozwałkuj ciasto i wykrawaj dowolne kształty za pomocą foremek.',
+      'Na środek każdego kształtu nałóż farsz.',
+      'Zamknij pierogi, używając widelca do zaciśnięcia brzegów.',
+      'Gotuj w parowarze przez 10 minut.'
+    ]
+  ],
+  serving: [
+    [
+      'Podawaj pierogi gorące, polane roztopionym masłem.',
+      'Możesz również podać je z cebulką podsmażoną na złoty kolor.',
+      'Pierogi świetnie komponują się z kwaśną śmietaną.'
+    ],
+    [
+      'Udekoruj pierogi świeżymi ziołami, takimi jak koperek lub pietruszka.',
+      'Podawaj z sosem grzybowym lub mięsnym.',
+      'Dodaj kwaśną śmietanę dla złagodzenia smaku.'
+    ],
+    [
+      'Podawaj pierogi z chrupiącym boczkiem.',
+      'Dodaj sałatkę z surowych warzyw, aby zrównoważyć smaki.',
+      'Spróbuj z sosem pomidorowym lub pesto.'
+    ],
+    [
+      'Podawaj pierogi z dodatkiem świeżego sera twarogowego na wierzchu.',
+      'Spróbuj z sosem czosnkowym lub sosem tzatziki.',
+      'Pierogi są również pyszne z marynowanymi ogórkami.'
+    ],
+    [
+      'Podawaj pierogi z prażonymi ziarnami sezamu lub słonecznika.',
+      'Możesz również zaoferować pikantny sos chili do maczania.',
+      'Dodaj odrobinę świeżo zmielonego pieprzu dla intensywniejszego smaku.'
+    ]
+  ],
 }
+
+export default fakeDatabase
+
