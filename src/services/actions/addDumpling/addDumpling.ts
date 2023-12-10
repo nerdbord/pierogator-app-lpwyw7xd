f2665baa34b1e74ddf0886eb13b522a82082859b
JKapturkiewicz <kuba.kapek@gmail.com>
2023-12-10 14:08:54 +0100

    feat: implement saving dumpling

---
+++ src/services/actions/addDumpling/addDumpling.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import type { DumplingRecipe } from '@/types/types'
+
+export async function addDumpling(payload: DumplingRecipe) {
+  try {
+    const response = await fetch(
+      'https://training.nerdbord.io/api/v1/pierogator/dumpling-recipes',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${process.env.API_KEY}`,
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          Pragma: 'no-cache',
+          Expires: '0',
+        },
+        body: JSON.stringify(payload),
+      },
+    )
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const res = await response.json()
+    return res
+  } catch (error) {
+    console.error('Error adding new dumpling:', error)
+    throw new Error(`HTTP error! status: ${error}`)
+  }
+}
